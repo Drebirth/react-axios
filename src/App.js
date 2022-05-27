c0ddc9d12b67396d2fb166c9ed227d5782bd4b6a
drebirth <danielll98@outlook.com.br>
2022-05-27 11:48:36 -0300

    estudos com axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import api from './services/api';
 
 function App() {
+  const [user, setUser] = useState();
+  console.log(user);
+  useEffect(() =>{
+    api.get("/users/drebirth")
+    .then((response) => setUser(response.data))
+    .catch((err) => {
+      console.error("ops! erro no metodo")
+    });
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Usuário: {user?.login}</p>
+      <p>Biografia: {user?.bio}</p>
+      
+
+      
     </div>
   );
 }
